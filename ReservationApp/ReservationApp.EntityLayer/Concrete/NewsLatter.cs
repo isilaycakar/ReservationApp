d068564bced14d4f72e92fc1d197a334fb2e4d10
isilaycakar <118280073+isilaycakar@users.noreply.github.com>
2023-08-03 16:54:12 +0300

    mimarinin oluşturulması.

---
+++ ReservationApp/ReservationApp.EntityLayer/Concrete/NewsLatter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationApp.EntityLayer.Concrete
+{
+    public class NewsLatter
+    {
+        public int NewsLatterID { get; set; }
+        public string Mail { get; set; }
+    }
+}
