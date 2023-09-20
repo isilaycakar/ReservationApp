6e917e018cc840a96ee35bb90082fa0641b1afe8
isilaycakar <118280073+isilaycakar@users.noreply.github.com>
2023-09-21 02:01:08 +0300

    admin paneli sidebar düzenlemeleri

---
+++ ReservationApp/ReservationApp.Panel.UI/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReservationApp.Panel.UI.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialHeader()
+        {
+
+            return PartialView();
+        }
+
+        public PartialViewResult PartialAppBrandDemo()
+        {
+
+            return PartialView();
+        }
+
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+    }
+}
