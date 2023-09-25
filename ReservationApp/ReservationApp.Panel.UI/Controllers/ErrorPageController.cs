b5b1b393a9685951529f0a5c1f032f0928253ef6
isilaycakar <118280073+isilaycakar@users.noreply.github.com>
2023-09-25 14:22:07 +0300

    deneme

---
+++ ReservationApp/ReservationApp.Panel.UI/Controllers/ErrorPageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReservationApp.Panel.UI.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        public IActionResult Error404(int code)
+        {
+
+            return View();
+        }
+    }
+}
