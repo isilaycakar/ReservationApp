5cd9798990a87ee21871a9e6b7bd8c181ec71437
isilaycakar <118280073+isilaycakar@users.noreply.github.com>
2023-10-01 22:44:18 +0300

    DTO katmanının eklenmesi

---
+++ ReservationApp/DTOLayer/DTOs/AppUserDTOs/AppUserRegisterDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOLayer.DTOs.AppUserDTOs
+{
+    public class AppUserRegisterDTO
+    {
+        public string NameSurname { get; set; }
+        public string UserName { get; set; }
+        public string Mail { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ ReservationApp/ReservationApp.BusinessLayer/ValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,25 @@
+﻿using DTOLayer.DTOs.AppUserDTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationApp.BusinessLayer.ValidationRules
+{
+    public class AppUserRegisterValidator: AbstractValidator<AppUserRegisterDTO>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x=>x.NameSurname).NotEmpty().WithMessage("Ad Soyad alanı boş geçilemez.");
+            RuleFor(x=>x.UserName).NotEmpty().WithMessage("Kullanıcı adı alanı boş geçilemez.");
+            RuleFor(x=>x.Mail).NotEmpty().WithMessage("Mail alanı boş geçilemez.");
+            RuleFor(x=>x.Password).NotEmpty().WithMessage("Şifre alanı boş geçilemez.");
+            RuleFor(x=>x.ConfirmPassword).NotEmpty().WithMessage("Şifre tekrar alanı boş geçilemez.");
+            RuleFor(x => x.UserName).MinimumLength(5).WithMessage("lütfen en az 5 karakter veri girişi yapınız");
+            RuleFor(x => x.UserName).MaximumLength(20).WithMessage("lütfen en fazla 20 karakter veri girişi yapınız");
+            RuleFor(x => x.Password).Equal(y => y.ConfirmPassword).WithMessage("şifreler birbiriyle uyuşmuyor");
+        }
+    }
+}
